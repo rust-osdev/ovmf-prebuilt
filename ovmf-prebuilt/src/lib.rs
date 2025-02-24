851b52d5281860952dc89021a093d4d90813e93f
Nicholas Bishop <nbishop@nbishop.net>
2025-02-24 18:48:25 -0500

    Add 202408.01, 202411, and 202502 releases

--- ovmf-prebuilt/src/lib.rs
+++ ovmf-prebuilt/src/lib.rs
@@ -36,11 +36,26 @@ pub struct Source {
     pub sha256: &'static str,
 }
 
+#[allow(missing_docs)]
 impl Source {
-    #[allow(missing_docs)]
     pub const EDK2_STABLE202408_R1: Self = Self {
         tag: "edk2-stable202408-r1",
         sha256: "63a9217ddd51fa45d0a89fd83c483cc971765de6bb08e83cf70836b0baff0d48",
+    };
+
+    pub const EDK2_STABLE202408_01_R1: Self = Self {
+        tag: "edk2-stable202408.01-r1",
+        sha256: "1b4c7d7603517482a3c4461ba43044c4c7e0a7930274d77eb19600b7dcd9b838",
+    };
+
+    pub const EDK2_STABLE202411_R1: Self = Self {
+        tag: "edk2-stable202411-r1",
+        sha256: "963fc6cef6a0560cec97381ed22a7d5c76f440c8212529a034cb465466cd57cc",
+    };
+
+    pub const EDK2_STABLE202502_R1: Self = Self {
+        tag: "edk2-stable202502-r1",
+        sha256: "6d6122e88cdc09e1ffafb6a39fbdbfba668a6ded3f2a032b2cd6c0b7ff6d69df",
     };
 
     /// Latest release tag.
